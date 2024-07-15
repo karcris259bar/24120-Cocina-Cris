fc62dc5e6be24f7953aea296647afc5f60aab574
karcris259bar <147669491+karcris259bar@users.noreply.github.com>
2024-07-15 20:56:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const container = document.querySelector(".container");
-const btnSignIn = document.getElementById("btn-sign-in");
-const btnSignUp = document.getElementById("btn-sign-up");
-btnSignIn.addEventListener("click",()=>{
-   container.classList.remove("toggle");
-});
-btnSignUp.addEventListener("click",()=>{
-   container.classList.add("toggle");
-});
+// const container = document.querySelector(".container");
+// const btnSignIn = document.getElementById("btn-sign-in");
+// const btnSignUp = document.getElementById("btn-sign-up");
+// btnSignIn.addEventListener("click",()=>{
+//    container.classList.remove("toggle");
+// });
+// btnSignUp.addEventListener("click",()=>{
+//    container.classList.add("toggle");
+// });
